3cdf0b68be66e609cf97d27886685bd69ad5ed02
mvikasgithub <vikasm@gmail.com>
2017-05-30 04:19:09 -0700

    First Four programs in Hadoop

---
+++ GenderSalCount.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class GenderSalCount 
+{
+
+	public static class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable>
+	{
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
+		{
+			String[] arr = value.toString().split(" ");
+			
+			context.write(new Text(arr[1]), new IntWritable(Integer.parseInt(arr[2])));
+		}
+	}
+	public static class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable>
+	{
+		public void reduce(Text key, Iterable<IntWritable> value, Context context) throws IOException, InterruptedException
+		{
+			int sum = 0;
+			for(IntWritable val:value)
+			{
+				sum += val.get();
+			}
+			context.write(key, new IntWritable(sum));
+		}
+	}
+		
+	
+	/**
+	 * @param args
+	 * @throws IOException 
+	 * @throws InterruptedException 
+	 * @throws ClassNotFoundException 
+	 */
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException 
+	{
+		Configuration cfg = new Configuration();
+		Job job = Job.getInstance(cfg, "GenderSalCount");
+		job.setJarByClass(GenderSalCount.class);
+		job.setMapperClass(MyMapper.class);
+		job.setReducerClass(MyReducer.class);
+		
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileSystem.get(cfg).delete(new Path(args[1]), true);
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);		
+
+	}
+
+}
